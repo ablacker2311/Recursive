18201674a821c8e5a90e21977975d9fbb44a5c14
ablacker2311 <qthong2004@gmail.com>
2023-07-26 17:05:59 +0700

    [QHĐ] tìm dãy con không giảm dài nhất, cập nhật tối ưu hơn

--- LIS.cpp
+++ LIS.cpp
@@ -1,3 +1,18 @@
+/*bài này là bài tìm dãy con không giảm dài nhất.
+hướng giải quyết => tạo ra 2 mảng, 1 mảng lưu chiều dài của mảng con không giảm dài nhất tới vị trí thứ i -> dp[i]
+                    mảng còn lại lưu vị trí của phần tử nằm trước phần tử có vị trí thứ i trong dãy không giảm -> posPrev[i]
+ta sử dụng 2 vòng lặp lồng nhau, tại vị trí thứ i thì t xét tất cả các phần tử trước i có thỏa điều kiện là 
+arr[i] >= arr[j] và dp[i] < dp[j] + 1 (xét xem độ dài của dãy tại vị trí i có nhỏ hơn độ dài của dãy tại vị trí thứ j + 1 hay không )
+điều kiện cơ sở : dp[i] = 1; ( ban đầu t giả sử dãy không giảm có 1 phần tử thì tại mọi vị trí thứ i độ dài đều bằng 1)
+    sau khi xét xong các điều kiện trên, ta xét từng phần tử của mảng dp[i] để tìm ra độ dài chuỗi con dài nhất là csMax =dp[i]
+ và phần tử cuối cùng của chuỗi con là phần tử arr[i]. (end_index = i)
+    sau đó tạo mảng phụ kq[] có csMax phần tử và chạy vòng lặp từ 0-> csMax - 1;
+    với kq[csMax - 1] = arr[end_index];
+        end_index = posPrev[end_index];
+
+        cuối cùng in ra mảng kq[csMax];
+*/
+
 #define MAX 100001
 #include<iostream>
 
@@ -37,7 +52,6 @@ int main(){
     }
     int kq[MAX];
     int temp = csMax;
-    int k = 0;
     while (temp >= 0){
         kq[temp-1] = arr[end_index];
         end_index = posPrev[end_index];
